45b89be81ad888e8256348634b2b3a517a8e5445
Arkadip <in2arkadipb13@gmail.com>
2019-09-11 15:55:47 +0530

    Modal  added
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- src/conponent/HomeContactButtons/index.js
+++ src/conponent/HomeContactButtons/index.js
@@ -1,23 +1,68 @@
 import React from 'react';
-import { Grid, Button } from '@material-ui/core';
+import { Grid, Button, Modal, Fade, Backdrop } from '@material-ui/core';
 import ArrowDownwardRounded from '@material-ui/icons/ArrowDownwardRounded';
 
 class HomeContactButtons extends React.Component {
+    state = { open: false }
+
+    onGitButtonClick = (e) => {
+        e.preventDefault();
+        console.log('Modal Open')
+        this.setState({ open: true })
+    }
+
+    OnModalClose = () => {
+        this.setState({ open: false })
+    }
+
+
+    onResButtonClick = (e) => {
+        e.preventDefault();
+        window.open('')
+    }
+
     render() {
+
+        const { open } = this.state;
+
         return (
-            <Grid container spacing={5} justify="center" style={{ paddingBottom: '3rem' }}>
-                <Grid item>
-                    <Button>
-                        Get in Touch
+            <React.Fragment>
+                <Grid container spacing={5} justify="center" style={{ paddingBottom: '3rem' }}>
+                    <Grid item>
+                        <Button variant="outlined" onClick={this.onGitButtonClick}>
+                            Get in Touch
                     </Button>
-                </Grid>
-                <Grid item>
-                    <Button >
-                        <ArrowDownwardRounded />
-                        Resume
+                    </Grid>
+                    <Grid item>
+                        <Button variant="outlined" onClick={this.onResButtonClick}>
+                            <ArrowDownwardRounded />
+                            Resume
                     </Button>
+                    </Grid>
                 </Grid>
-            </Grid>
+
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    open={open}
+                    onClose={this.OnModalClose}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                        timeout: 500,
+                    }}
+
+                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                >
+
+                    <Fade in={open}>
+                        <div>
+                            <h2 id="transition-modal-title">Transition modal</h2>
+                            <p id="transition-modal-description">react-transiton-group animates me.</p>
+                        </div>
+                    </Fade>
+                </Modal>
+            </React.Fragment>
         )
     }
 }
