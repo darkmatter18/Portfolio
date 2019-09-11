db4138ca32cf3ebe130545861cdb1354be04714d
Arkadip <in2arkadipb13@gmail.com>
2019-09-11 19:38:45 +0530

    disabled button added
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- src/conponent/HomeModalForm/index.js
+++ src/conponent/HomeModalForm/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 class HomeModalForm extends React.Component {
 
-    state = { name: '', email: '', number: '', textmsg: '', submitted: false }
+    state = { name: '', email: '', number: '', textmsg: '', submitting: false, submitted: false, buttonBackgroundColor: '#00ad45' }
 
     handleChange = name => e => {
         e.preventDefault();
@@ -13,27 +13,28 @@ class HomeModalForm extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
+        this.setState({ submitting: true, buttonBackgroundColor: '#dbdbdb' })
         this.submitRequest()
     }
 
     submitRequest = async () => {
-            const response = await axios.post('http://www.google.com', {
-                git_name: this.state.name,
-                git_email: this.state.email,
-                git_mob: this.state.number,
-                git_msg: this.state.textmsg
-            });
-        
+        const response = await axios.post('http://www.google.com', {
+            git_name: this.state.name,
+            git_email: this.state.email,
+            git_mob: this.state.number,
+            git_msg: this.state.textmsg
+        });
+
         if (response.status === 200) {
             if (response.data.status === 'Y') {
-                this.setState({submitted: true})
+                this.setState({ submitted: true })
             }
         }
-
+        
     }
 
     renderForm() {
-        const { name, email, number, textmsg } = this.state
+        const { name, email, number, textmsg, submitting, buttonBackgroundColor } = this.state
         return (
             <form style={{ paddingTop: '1rem', paddingBottom: '2rem' }} onSubmit={this.handleSubmit}>
                 <TextField
@@ -81,7 +82,8 @@ class HomeModalForm extends React.Component {
 
                 <Button
                     type="submit"
-                    style={{ backgroundColor: '#00ad45', color: '#ffffff', float: 'right' }}
+                    style={{ backgroundColor: buttonBackgroundColor, color: '#ffffff', float: 'right' }}
+                    disabled={submitting}
                 >
                     Submit
                 </Button>
@@ -91,7 +93,7 @@ class HomeModalForm extends React.Component {
 
     renderSubmitted() {
         return (
-            <Typography variant="body2" align="left" style={{padding: '1rem'}}>
+            <Typography variant="body2" align="left" style={{ padding: '1rem' }}>
                 Your Request is sumbmitted. <br />
                 I'll reply you as soon as possible
             </Typography>
