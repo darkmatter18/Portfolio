8a82aa97a12d043d5cf513ac449141d139888794
Arkadip <in2arkadipb13@gmail.com>
2020-01-02 18:17:11 +0530

    feat: Backend files added

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+//const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// IMPORT MODELS
+//require('./models/Product');
+
+
+const app = express();
+
+//mongoose.Promise = global.Promise;
+//mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+
+app.use(bodyParser.json());
+
+//IMPORT ROUTES
+require('./routes/dummyRoute')(app);
+//require('./routes/productRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+});
