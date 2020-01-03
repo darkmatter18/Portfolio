80378560b1300e512d8f470fdfcf0778bc9716ef
Arkadip <in2arkadipb13@gmail.com>
2020-01-03 22:50:41 +0530

    rerun
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    recapta: '6LcGQncUAAAAAC567n0sA2qi_7ebg3QoZmPxBX1K',
-    host: 'mail.arkadip.co',
-    port: 465,
-    user: 'no-reply@arkadip.co',
-    pass: 'WV?8n&WTOuNw',
-    replyto: 'in2arkadipb13@gmail.com'
-}
