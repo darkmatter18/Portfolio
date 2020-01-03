e5fcaba8e03bd7ba1f3e1e50f9e821331d2c4897
Arkadip <in2arkadipb13@gmail.com>
2020-01-03 22:55:38 +0530

    feat: chaanged the build dir structure
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ app.use(bodyParser.json());
 route(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('public'));
 
   const path = require('path');
   app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
   })
 
 }
