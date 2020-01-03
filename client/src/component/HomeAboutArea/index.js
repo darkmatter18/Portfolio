5ce329d8b45feb19d64a15a8301c4e917962b8cf
Arkadip <in2arkadipb13@gmail.com>
2020-01-03 19:58:38 +0530

    feat: dir structure changed
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- client/src/component/HomeAboutArea/index.js
+++ client/src/component/HomeAboutArea/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Typography, } from '@material-ui/core';
 
-import logo from './logo.jpeg';
+import logo from './../../images/logo.jpeg';
 import HomeContactButtons from '../HomeContactButtons';
 import { WEBSITE_LINK, EMAIL_ID } from './../../utils/config';
 
