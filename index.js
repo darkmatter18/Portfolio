4311f384d21958f401a81fad46c60783f75fa2b9
Arkadip <in2arkadipb13@gmail.com>
2020-01-15 15:51:37 +0530

    setup proxy for gcp
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const emailRoute = require('./routes/emailRoute');
 const app = express();
 mongoose.Promise = global.Promise;
 
+app.set('trust proxy', true);
+
 let PORT, mongoStr;
 if (process.env.NODE_ENV === 'production') {
   PORT = process.env.PORT;
   mongoStr = `mongodb+srv://${process.env.mongouser}:${process.env.mongopass}@${process.env.mongoConnect}?${process.env.mongoQuery1}&${process.env.mongoQuery2}`;
 
-  app.use(express.static('public'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
