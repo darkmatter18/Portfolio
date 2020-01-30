c0899603fa1e51f235250021437100aee9359b86
Arkadip <in2arkadipb13@gmail.com>
2020-01-30 20:11:47 +0530

    fix: env variable fix
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- envset.sh
+++ envset.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo ' recapta: '$recapta >> app.yaml
-echo ' mail_host: '$mail_host >> app.yaml
-echo ' mail_port: '$mail_port >> app.yaml
-echo ' mail_user: '$mail_user >> app.yaml
-echo ' mail_pass: '$mail_pass >> app.yaml
-echo ' mail_secure: '$mail_secure >> app.yaml
-echo ' mail_replyto: '$mail_replyto >> app.yaml
-echo ' mongoConnect: '$mongoConnect >> app.yaml
-echo ' mongouser: '$mongouser >> app.yaml
-echo ' mongopass: '$mongopass >> app.yaml
+echo ' recapta: '${recapta} >> app.yaml
+echo ' mail_host: '${mail_host} >> app.yaml
+echo ' mail_port: '${mail_port} >> app.yaml
+echo ' mail_user: '${mail_user} >> app.yaml
+echo ' mail_pass: '${mail_pass} >> app.yaml
+echo ' mail_secure: '${mail_secure} >> app.yaml
+echo ' mail_replyto: '${mail_replyto} >> app.yaml
+echo ' mongoConnect: '${mongoConnect} >> app.yaml
+echo ' mongouser: '${mongouser} >> app.yaml
+echo ' mongopass: '${mongopass} >> app.yaml
