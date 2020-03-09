4e23d2bbcc755766bb3252ef959e3446e64bf4eb
Arkadip <in2arkadipb13@gmail.com>
2020-03-10 00:36:47 +0530

    Removed the github button
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- client/src/__tests__/HomeAboutArea.js
+++ client/src/__tests__/HomeAboutArea.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HomeAboutArea from "../component/HomeAboutArea";
 
-it('Render Upper Component without crashing', () => {
+it('Render About Component without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<HomeAboutArea />, div);
 });
