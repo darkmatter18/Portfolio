8d2ee856731fb6f36aa0d10ce3ec2b6128163cfd
Arkadip <in2arkadipb13@gmail.com>
2020-03-10 00:35:27 +0530

    Changed app test dir
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- client/src/__tests__/App.js
+++ client/src/__tests__/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './../App';
 
 // test('renders learn react link', () => {
 //   const { getByText } = render(<App />);
