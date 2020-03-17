00c1b8a8671599ee564f6a99c24c5d33b97079cf
Arkadip <in2arkadipb13@gmail.com>
2020-03-17 21:35:13 +0530

    feat: added send grid and configure it
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- utils/mailConfig.js
+++ utils/mailConfig.js
@@ -1,32 +1,14 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail')
 
-let mail_host, mail_port, mail_user, mail_pass, mail_replyto, mail_secure;
+let SENDGRID_API;
 if (process.env.NODE_ENV === 'production') {
-    mail_host = process.env.mail_host;
-    mail_port = process.env.mail_port;
-    mail_user = process.env.mail_user;
-    mail_pass = process.env.mail_pass;
-    mail_replyto = process.env.mail_replyto;
-    mail_secure = process.env.mail_secure;
+    SENDGRID_API = process.env.SENDGRID_API
 }
 else {
     const config = require('./../devconfig');
-    mail_host = config.mail_host;
-    mail_port = config.mail_port;
-    mail_user = config.mail_user;
-    mail_pass = config.mail_pass;
-    mail_replyto = config.mail_replyto;
-    mail_secure = config.mail_secure;
+    SENDGRID_API = config.SENDGRID_API;
 }
 
-const transporter = nodemailer.createTransport({
-    host: mail_host,
-    port: mail_port,
-    secure: mail_secure,
-    auth: {
-        user: mail_user,
-        pass: mail_pass
-    }
-});
+sgMail.setApiKey(SENDGRID_API);
 
-module.exports = transporter;
+module.exports = sgMail;
