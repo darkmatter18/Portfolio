1be84cdf0ac53f55afaf08859dc8653a0424df72
Arkadip <in2arkadipb13@gmail.com>
2020-05-01 11:04:55 +0530

    feat: licence added
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,6 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import green from '@material-ui/core/colors/green';
-import blue from '@material-ui/core/colors/blue';
+
 const theme = createMuiTheme({
     palette: {
         secondary: {
