31b46532c6c0912732f9f25a25b162ae3502056d
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-05-12 19:59:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+  });
 }
 else {
   const config = require('./devconfig');
@@ -40,7 +40,7 @@ mongoose.connect(mongoStr, { useUnifiedTopology: true, useNewUrlParser: true })
 app.use(bodyParser.json());
 
 //IMPORT ROUTES
-app.use(`/api`, emailRoute)
+app.use(`/api`, emailRoute);
 
 app.listen(PORT, () => {
   console.log(`app running on http://localhost:${PORT}`)
