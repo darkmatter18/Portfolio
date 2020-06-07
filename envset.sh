fbde94168f4feb97616ec19ae41f70721537bcf4
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-07 13:16:27 +0530

    feat: envset modified as per github
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- envset.sh
+++ envset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo '
- recapta: '${recapta} >> app.yaml
-echo ' MAIL_SENDER: '${MAIL_SENDER} >> app.yaml
-echo ' MAIL_REPLYTO: '${MAIL_REPLYTO} >> app.yaml
-echo ' SENDGRID_API: '${SENDGRID_API} >> app.yaml
-echo ' mongoConnect: '${mongoConnect} >> app.yaml
-echo ' mongouser: '${mongouser} >> app.yaml
-echo ' mongopass: '${mongopass} >> app.yaml
+ recapta: '${{ serects.RECAPTA_PRIVATE_KEY }} >> app.yaml
+echo ' MAIL_SENDER: '${{ serects.MAIL_SENDER }} >> app.yaml
+echo ' MAIL_REPLYTO: '${{ serects.MAIL_REPLYTO }} >> app.yaml
+echo ' SENDGRID_API: '${{ serects.SENDGRID_API }} >> app.yaml
+echo ' mongoConnect: '${{ serects.MONGODB_CONNECT }} >> app.yaml
+echo ' mongouser: '${{ serects.MONGODB_USER }} >> app.yaml
+echo ' mongopass: '${{ serects.MONGODB_PASS }} >> app.yaml
