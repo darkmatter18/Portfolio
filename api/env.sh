376150094c6629d9e20c5418a01852ed9e9c098d
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-21 20:18:23 +0530

    fix: env changed

--- api/env.sh
+++ api/env.sh
@@ -1,10 +1,10 @@
 echo '
- recapta: '${{ secrets.RECAPTA_PRIVATE_KEY }} >> ./api/app.yaml
-echo ' MAIL_SENDER: '${{ secrets.MAIL_SENDER }} >> ./api/app.yaml
-echo ' MAIL_REPLYTO: '${{ secrets.MAIL_REPLYTO }} >> ./api/app.yaml
-echo ' SENDGRID_API: '${{ secrets.SENDGRID_API }} >> ./api/app.yaml
-echo ' mongoConnect: '${{ secrets.MONGODB_CONNECT }} >> ./api/app.yaml
-echo ' mongouser: '${{ secrets.MONGODB_USER }} >> ./api/app.yaml
-echo ' mongopass: '${{ secrets.MONGODB_PASS }} >> ./api/app.yaml
-echo ' serect: '${{ secrets.JWT_SERECT }} >> ./api/app.yaml
+ recapta: '${{ secrets.RECAPTA_PRIVATE_KEY }} >> app.yaml
+echo ' MAIL_SENDER: '${{ secrets.MAIL_SENDER }} >> app.yaml
+echo ' MAIL_REPLYTO: '${{ secrets.MAIL_REPLYTO }} >> app.yaml
+echo ' SENDGRID_API: '${{ secrets.SENDGRID_API }} >> app.yaml
+echo ' mongoConnect: '${{ secrets.MONGODB_CONNECT }} >> app.yaml
+echo ' mongouser: '${{ secrets.MONGODB_USER }} >> app.yaml
+echo ' mongopass: '${{ secrets.MONGODB_PASS }} >> app.yaml
+echo ' serect: '${{ secrets.JWT_SERECT }} >> app.yaml
 cat app.yaml
