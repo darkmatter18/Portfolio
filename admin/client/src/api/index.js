cd0e44bf07330a54e2db12565bf03733c29fb73d
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-21 18:36:12 +0530

    feat: multiple error solved

--- admin/client/src/api/index.js
+++ admin/client/src/api/index.js
@@ -1,17 +1,9 @@
 import axios from 'axios';
-import {authHeader} from "jwt-auth-react";
 
 const baseURL = 'https://api.arkadip.co'
 
-const authApi = axios.create({
-    baseURL: baseURL,
-    headers: {
-        'Authentication': authHeader()
-    }
-})
-
-const authLessApi = axios.create({
+const Api = axios.create({
     baseURL: baseURL
 })
 
-export {authApi, authLessApi}
+export {Api}
--- admin/client/src/pages/Login/index.js
+++ admin/client/src/pages/Login/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import {authLessApi} from "../../api";
+import React, {} from "react";
+import {Api} from "../../api";
 
 const Login = () => {
 
-    const [idState, setIdState] = React.useContext(null)
-    const [passwordState, setPasswordState] = React.useContext(null)
+    const [idState, setIdState] = React.useState(null)
+    const [passwordState, setPasswordState] = React.useState(null)
 
     const formSubmit = async (e) => {
         e.preventDefault()
         try{
-            const res = await authLessApi.post('/i/login',{id: idState, password: passwordState})
+            const res = await Api.post('/i/login',{id: idState, password: passwordState})
             if(res.status === 200) {
                 console.log(res.data)
             }
@@ -26,8 +26,8 @@ const Login = () => {
     return (
         <React.Fragment>
             <form onSubmit={formSubmit}>
-                <input type="text" name="id" onChange={(e) => {setIdState(e.target.value)}}/>
-                <input type="password" name="password" onChange={(e => {setPasswordState(e.target.value)})}/>
+                <input type="text" name="id" onChange={(e) => {setIdState(e.target.value)}} value={idState}/>
+                <input type="password" name="password" onChange={(e => {setPasswordState(e.target.value)})} value={passwordState}/>
                 <button type={"submit"}>Submit</button>
             </form>
         </React.Fragment>
