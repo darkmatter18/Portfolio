8d84ffd1b30b1afede7d8322d1f88b36edbffad1
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-21 19:05:50 +0530

    fix: route

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,7 +1,7 @@
 const authRoute = require('express').Router()
 const jwt = require('jsonwebtoken')
 
-authRoute.post(`login`, async (req, res) => {
+authRoute.post(`/login`, async (req, res) => {
     const {id, password} = req.body
     const _jwt = jwt.sign(`${id}---${password}`,process.env.serect, {expiresIn: '2h'})
     res.json({_jwt, expiresIn: 2*60})
