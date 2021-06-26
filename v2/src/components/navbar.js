4635ab99df3da498111e7624ae3a175259001585
Arkadip <in2arkadipb13@gmail.com>
2021-06-26 19:46:37 +0530

    🧭 [NavBar] added Nav links

--- v2/src/components/navbar.js
+++ v2/src/components/navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import LoaderIcon from "../icons/LoaderIcon";
+import NavButton from "./navButton";
 
 const Navbar = () => {
+  const navLinks = [{name: '01. About', url: "#about"}]
   const icon = (
-    <div className={"w-12"}>
+    <div className={"w-12 m-0 float-left"}>
       <LoaderIcon isOnLoader={false}/>
     </div>
   )
@@ -11,6 +13,13 @@ const Navbar = () => {
   return (
     <div className={"pt-4 pl-6 pr-6"}>
       {icon}
+      <div className={"float-right"}>
+        {navLinks.map((navLink, i) => {
+          return (
+            <NavButton key={i} url={navLink.url} name={navLink.name}/>
+          )
+        })}
+      </div>
     </div>
   )
 }
