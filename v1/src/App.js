60c2510ee21cd30399d22f8b59710b1b040ddab0
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2021-09-27 21:00:59 +0530

    refactor(adsence): moved from the index.html to app.js

--- v1/src/App.js
+++ v1/src/App.js
@@ -5,6 +5,14 @@ import RouteComponent from './routes';
 import theme from './theme';
 
 class App extends React.Component {
+  componentDidMount() {
+    //SetAdsense
+    let script = document.createElement('script');
+    script.setAttribute('data-ad-client', 'ca-pub-4077277774959103');
+    script.setAttribute('async');
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
   render() {
     return (
       <React.Fragment>
