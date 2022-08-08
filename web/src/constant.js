b8a36e26d27b34deb523a0e0708cca2d04192cbd
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2022-08-08 22:17:23 +0530

    feat: updated the resume link

--- web/src/constant.js
+++ web/src/constant.js
@@ -1,7 +1,7 @@
 export const EMAIL_ID           = 'in2arkadipb13@gmail.com';
 export const WEBSITE_LINK       = 'https://www.arkadip.me';
 
-export const RESUME_LINK        = 'https://drive.google.com/file/d/1pq-pUboG3AVPW2_iAnwQgnJriaKW0xnT/view?usp=sharing';
+export const RESUME_LINK        = 'https://drive.google.com/file/d/1HP_p0MKgdNSHeqf-rCsYYsv2IKZgAj4_/view?usp=sharing';
 export const GITHUB_LINK        = 'https://github.com/darkmatter18';
 export const LINKEDIN_LINK      = 'https://www.linkedin.com/in/arkadip';
 export const FACEBOOK_LINK      = 'https://facebook.com/arkadipb';
